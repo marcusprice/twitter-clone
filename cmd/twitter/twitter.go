593cc5bc52e5bbf7dc93c8d28e24bb83fc93298e
Marcus Price <marcusprice88@gmail.com>
2025-06-08 00:37:42 -0700

    updates .gitignore to allow cmd/main package

---
+++ cmd/twitter/twitter.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"slices"
+
+	"github.com/marcusprice/twitter-clone/internal/api"
+	"github.com/marcusprice/twitter-clone/internal/constants"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	dbPath := os.Getenv("DB_PATH")
+	env := os.Getenv("ENV")
+	envs := []string{constants.DEV_ENV, constants.PROD_ENV}
+
+	fmt.Println(os.Getenv("HOST"))
+	fmt.Println(os.Getenv("PORT"))
+	fmt.Println(os.Getenv("DB_PATH"))
+	fmt.Println(os.Getenv("ENV"))
+
+	runtime.Breakpoint()
+
+	if env == "" {
+		panic("ENV environment variable required")
+	} else if !slices.Contains(envs, env) {
+		panic("Invalid ENV environment variable set, must be 'DEVELOPMENT' or 'PRODUCTION'")
+	}
+
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	if port == "" {
+		port = "5000"
+	}
+
+	if dbPath == "" {
+		panic("DB path required")
+	}
+
+	conn, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		panic(err)
+	}
+	handler := api.RegisterHandlers(conn)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), handler))
+}
